535d757382733e2af962cbd5619d57bab5a11d72
Charles Agoe <agoetee@gmail.com>
2025-05-13 17:33:31 +0000

    add values for a user

---
+++ database-script-0x02/README.md
@@ -0,0 +1,3 @@
+# SQL Commands
+
+This directory has sql file to insert information into the tables
---
+++ database-script-0x02/seed.sql
@@ -0,0 +1,2 @@
+INSERT INTO User(first_name, last_name, email, phone_number, role, created_at)
+VALUES ('Kofi', 'Nti','kofi.nti@aboamail.com', 'admin',2025-04-30 13:10:02.047)
